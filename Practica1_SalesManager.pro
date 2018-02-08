138e9413f58ca4b419b75dfab634482d6a4c7f91
MacoChave <totto_cha@hotmail.com>
2018-02-08 08:25:42 -0600

    Selector de archivo json

--- Practica1_SalesManager.pro
+++ Practica1_SalesManager.pro
@@ -13,8 +13,10 @@ TEMPLATE = app
 
 
 SOURCES += main.cpp\
-        mainwindow.cpp
+        mainwindow.cpp \
+    Parser/json.c
 
-HEADERS  += mainwindow.h
+HEADERS  += mainwindow.h \
+    Parser/json.h
 
 FORMS    += mainwindow.ui
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,19 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_actionCargar_JSON_triggered()
+{
+    QString filename = QFileDialog::getOpenFileName(
+                this,
+                "Seleccionar archivo",
+                "/home/marco/Escritorio/",
+                "JSON files(*.JSON);;JSON files(*.json)"
+                );
+    QMessageBox::information(this, "File name", filename);
+}
+
+void MainWindow::on_actionExportar_JSON_triggered()
+{
+
+}
